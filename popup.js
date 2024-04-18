6dbf157b4f070ed6027879e857bc6611897170dd
nautilo <8lapse@gmail.com>
2024-04-18 01:05:24 -0400

    script bloqueo url actualizado

--- popup.js
+++ popup.js
@@ -1,6 +1,25 @@
 // Importar el módulo opennsfw2
 import * as opennsfw2 from 'opennsfw2'; // Asegúrate de que opennsfw2 esté correctamente instalado
 
+
+// Realizar una solicitud GET a la API para obtener los sitios bloqueados
+fetch('http://127.0.0.1:8000/api/sitiosbloqueados/')
+  .then(response => response.json())
+  .then(data => {
+    const sitiosBloqueados = data.map(sitio => sitio.url);
+
+    // Obtener el URL actualmente visitado
+    const urlActual = window.location.href;
+
+    // Comparar el URL actual con los sitios bloqueados
+    if (sitiosBloqueados.includes(urlActual)) {
+      // Si hay una coincidencia, detener la carga de la página y mostrar un mensaje
+      window.stop();
+      alert('Este sitio está bloqueado por el supervisor.');
+    }
+  })
+  .catch(error => console.error('Error al obtener los sitios bloqueados:', error));
+
 // Umbral de probabilidad NSFW
 const NSFW_THRESHOLD = 0.5;
 
